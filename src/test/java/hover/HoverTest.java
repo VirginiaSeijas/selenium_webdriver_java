19246025a7180b37346ed735b6a2f8353e90f1b8
Josalys <jvmatoss@falabella.cl>
2021-02-23 15:31:24 -0300

    litle change to test

--- src/test/java/hover/HoverTest.java
+++ src/test/java/hover/HoverTest.java
@@ -13,7 +13,7 @@ public class HoverTest  extends BaseTest {
         var hoversPage = homePage.clickHovers();
         var caption = hoversPage.hoverOverFigure(1);
         assertTrue(caption.isCaptionDisplayed(), "Caption not displayed");
-        assertEquals(caption.getTitle(), "name: user1", "caption title incorrect");
+        assertEquals(caption.getTitle(), "name: user1", "Caption title incorrect");
         assertEquals(caption.getLinkText(), "View profile", "Caption link text incorrect");
         assertTrue(caption.getLink().endsWith("/users/1"), "Link incorrect");
     }
